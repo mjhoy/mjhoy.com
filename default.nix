1bbc829c6e544db248fd8f5650816ab7a80c5aca
Michael Hoy <mjh@mjhoy.com>
2015-09-25 16:34:58 -0500

    nix compat

---
+++ default.nix
@@ -0,0 +1,12 @@
+{ mkDerivation, base, hakyll, pandoc, stdenv }:
+mkDerivation {
+  pname = "blog";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  buildDepends = [ base hakyll pandoc ];
+  homepage = "http://mjhoy.com";
+  description = "mjhoy.com website";
+  license = stdenv.lib.licenses.mit;
+}
