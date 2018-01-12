e6a3774b8c1655cfcc27470812d79258e4dcf1cd
Michael Hoy <mjh@mjhoy.com>
2018-01-12 13:57:29 -0600

    add nix shell files

---
+++ shell-osx.nix
@@ -0,0 +1,38 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, filepath, hakyll, hxt, pandoc, stdenv
+      , time, time-locale-compat
+      }:
+      mkDerivation {
+        pname = "blog";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        # Needed for OSX
+        executableSystemDepends = [
+          pkgs.darwin.apple_sdk.frameworks.Cocoa
+        ];
+        executableHaskellDepends = [
+          base filepath hakyll hxt pandoc time time-locale-compat
+        ];
+        homepage = "http://mjhoy.com";
+        description = "mjhoy.com website";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
